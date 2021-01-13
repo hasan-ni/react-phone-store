2239139abf133b497d659b76d383096d9573c896
hasan-ni <nimal.hassan@gmail.com>
2021-01-13 17:35:50 +0500

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import logo from '../logo.svg'
+import styled from 'styled-components'
+import { ButtonContainer } from './Button'
+export default class Navbar extends Component {
+  render() {
+    return (
+      <NavWrapper className='navbar navbar-expand-sm navbar-dark px-sm-6'>
+        <Link to='/'>
+          <img src={logo} alt='store' className='navbar-brand' />
+        </Link>
+        <ul className='navbar-nav align-items-center'>
+          <li className='nav-item ml-5'>
+            <Link to='/' className='nav-link'>
+              Products
+            </Link>
+          </li>
+        </ul>
+        <Link to='/Cart' className='ml-auto'>
+          <ButtonContainer>
+            <i className='fa fa-cart-plus' />
+            &nbsp; My Cart
+          </ButtonContainer>
+        </Link>
+      </NavWrapper>
+    )
+  }
+}
+
+const NavWrapper = styled.nav`
+  background: var(--mainBlue);
+  .nav-link {
+    color: var(--mainWhite) !important;
+    font-size: 1.3rem;
+    text-transform: capitalize;
+  }
+`
